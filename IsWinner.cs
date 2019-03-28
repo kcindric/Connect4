bb998981bcc0a5dd4069d48361f04022dc51fcb0
Kristijan Cindrić <44549361+kcindric@users.noreply.github.com>
2019-03-28 18:58:52 +0100

    Update IsWinner.cs

--- IsWinner.cs
+++ IsWinner.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,21 +10,47 @@ namespace Cheese
     {
         int rows = 6;
         int col = 7;
-        int loopNumber = 0;
-        public void WinnerCheck(string[,] board, bool gameEnd, string playerSign)
+        bool gameEnd = false;
+
+        //checks
+        public bool WinnerCheck(string[,] board, string playerSign)
         {
-            gameEnd = false;
-            for(int i = 0; i < rows; i++)
+            for (int i = 0; i < rows; i++)
             {
                 for(int j = 0; j < col; j++)
                 {
-                    //ne radi jer gleda i * a ne samo X i O pa tako često ovo bude
+                    //ide do 9 elemenata zato jer računa i "|" dok mi baca error za vertikalni check jer nemam tih dodatnih znakova
+                    //neki workaraound gdje bi checkirao do sigurne duljine i onda promijenio stranu za ostatak
                     if (board[i,j]==playerSign && board[i,j+1] == playerSign && board[i,j+2] == playerSign && board[i, j + 3] == playerSign)
                     {
                         Console.WriteLine($"Winner {playerSign}!");
-                        gameEnd = false;
+                        gameEnd = true;
+                        
                     }
                 }
+            }
+
+            for (int j = 0; j < col; j++)
+            {
+                for (int i = 0; i < rows; i++)
+                {
+                    //izadje mi van arraya, trenutno ne znam zašto - pogledaj komentar za gornju petlju
+                    if (board[i, j] == playerSign && board[i + 1, j] == playerSign && board[i + 2, j] == playerSign && board[i + 3, j] == playerSign)
+                    {
+                        Console.WriteLine($"Winner {playerSign}!");
+                        gameEnd = true;
+
+                    }
+                }
+            }
+
+            if (gameEnd)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
             }
         }
     }
