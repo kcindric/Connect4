4f508df22d18ef4bde7aff8f7abc9da04cc45e9e
Kristijan Cindrić <44549361+kcindric@users.noreply.github.com>
2019-04-01 16:00:29 +0200

    Added L to R horizontal check

--- IsWinner.cs
+++ IsWinner.cs
@@ -8,33 +8,40 @@ namespace Cheese
 {
     public class IsWinner
     {
-        int rows = 6;
-        int col = 7;
         bool gameEnd = false;
 
         //checks
         public bool WinnerCheck(string[,] board, string playerSign)
         {
+            //horizontal check
+            int rows = 6;
+            int col = 7;
+
             for (int i = 0; i < rows; i++)
             {
-                for(int j = 0; j < col; j++)
+                for (int j = 0; j < col; j++)
                 {
-                    //ide do 9 elemenata zato jer računa i "|" dok mi baca error za vertikalni check jer nemam tih dodatnih znakova
-                    //neki workaraound gdje bi checkirao do sigurne duljine i onda promijenio stranu za ostatak
-                    if (board[i,j]==playerSign && board[i,j+1] == playerSign && board[i,j+2] == playerSign && board[i, j + 3] == playerSign)
+                    //it checks columns 
+                    if (board[i, j] == playerSign && board[i, j + 1] == playerSign && board[i, j + 2] == playerSign && board[i, j + 3] == playerSign)
                     {
                         Console.WriteLine($"Winner {playerSign}!");
                         gameEnd = true;
-                        
+
                     }
                 }
             }
 
-            for (int j = 0; j < col; j++)
+
+            //vertical check
+            //int rows and col are different because there are no additional "|" signs as in the horizontal check
+            int rows_vertical = 3;
+            int col_vertical = 7;
+
+            for (int j = 0; j < col_vertical; j++)
             {
-                for (int i = 0; i < rows; i++)
+                for (int i = 0; i < rows_vertical; i++)
+
                 {
-                    //izadje mi van arraya, trenutno ne znam zašto - pogledaj komentar za gornju petlju
                     if (board[i, j] == playerSign && board[i + 1, j] == playerSign && board[i + 2, j] == playerSign && board[i + 3, j] == playerSign)
                     {
                         Console.WriteLine($"Winner {playerSign}!");
@@ -43,6 +50,44 @@ namespace Cheese
                     }
                 }
             }
+
+            //diagonal check
+            #region diagonal check L to R
+            
+            
+
+            for(int i = 3; i < 6; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    //don't know should I put it in our outside with the int loop?
+                    //IT WORKS!
+                    //test every possible outcome
+                    int innerLoop = 0;
+                    int loop = 0;
+                    do
+                    {
+                        //index outside of the array
+                        if (board[i-loop, j+loop] == playerSign && board[i-1-loop, j+1+loop] == playerSign && board[i-2-loop, j+2+loop] == playerSign && board[i-3-loop, j+3+loop] == playerSign)
+                        {
+                            Console.WriteLine($"Winner {playerSign}!");
+                            gameEnd = true;
+                        }
+                        innerLoop++;
+                    } while (innerLoop < loop);
+                    loop++;
+                }
+            }
+           
+
+            if (board[3,0] == playerSign && board[2,1] == playerSign && board[1,2] == playerSign && board[0,3] == playerSign )
+            {
+                Console.WriteLine($"Winner {playerSign}!");
+                gameEnd = true;
+            }
+            #endregion
+
+
 
             if (gameEnd)
             {
